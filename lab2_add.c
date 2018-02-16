6660d4560e80504b22162bfe41903426589169f5
Clark Minor <clarkminor@g.ucla.edu>
2018-02-15 20:56:57 -0800

    Implement --sync=m successful

--- lab2_add.c
+++ lab2_add.c
@@ -7,31 +7,43 @@
 #include <stdio.h> //fprintf(), perror()
 #include <stdlib.h> //atoi()
 #include <time.h> //clock_gettime()
+#include <string.h> //strcat()
 
 #define BILLION 1000000000L
 int num_threads = 1;
 int num_iterations = 1;
 int opt_yield = 0;
-char * test_name = "add-none";
+int opt_sync_mutex = 0;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
-int opt_yield;
-        void add(long long *pointer, long long value) {
-                long long sum = *pointer + value;
-                if (opt_yield)
-                        sched_yield();
-                *pointer = sum;
-        }
+
+void add(long long *pointer, long long value)
+{
+  long long sum = *pointer + value;
+  if (opt_yield)
+        sched_yield();
+  *pointer = sum;
+}
 
 void * thread_func(void * counter)
 {
-  //perror("thread_func reached");
-  //fprintf(stderr,"num iterations = %d\n", num_iterations);
   int i=0;
   while(i < num_iterations)
   {
-    add((long long *) counter, (long long)  1);
-    add((long long *) counter, (long long) -1);
+    if(opt_sync_mutex)
+    {
+      pthread_mutex_lock(&mutex);
+      add((long long *) counter, (long long)  1);
+      add((long long *) counter, (long long) -1);
+      pthread_mutex_unlock(&mutex);
+    }
+    else
+    {
+      add((long long *) counter, (long long)  1);
+      add((long long *) counter, (long long) -1);
+    }
+
     i++;
   }
   return NULL;
@@ -48,11 +60,12 @@ int main(int argc, char ** argv)
     {"threads",    optional_argument, NULL, 't'},
     {"iterations", optional_argument, NULL, 'i'},
     {"yield",      no_argument,       NULL, 'y'},
+    {"sync",       required_argument, NULL, 's'},
     {0,0,0,0}
   };
 
   int opt = 0;
-  while((opt = getopt_long(argc, argv, "t:i:y", long_opts, NULL)) != -1)
+  while((opt = getopt_long(argc, argv, "t:i:ys:", long_opts, NULL)) != -1)
   {
     switch(opt)
 		{
@@ -68,10 +81,21 @@ int main(int argc, char ** argv)
 
       case 'y':
         opt_yield = 1;
-        test_name = "add-yield-none";
         break;
+
+      case 's':
+        if(optarg && *optarg == 'm')
+          opt_sync_mutex = 1;
+        else
+        {
+          fprintf(stderr, "Usage: --sync=[m]");
+          exit(1);
+        }
+        break;
+
       default:
-        fprintf(stderr, "Usage: [ --threads=# --iterations=# --yield ]");
+        fprintf(stderr, "Usage: [ --threads=# --iterations=# --yield --sync=[m] ]");
+        exit(1);
 
     }
   }
@@ -113,11 +137,26 @@ int main(int argc, char ** argv)
   */
   long long time_elapsed = BILLION * (end.tv_sec - start.tv_sec) +  end.tv_nsec - start.tv_nsec;
 
-
-
   long long num_operations = num_threads * num_iterations * 2;
 
   long long time_per_operation = time_elapsed / num_operations;
+
+  //determine the name of the test
+  char test_name[64] = "add";
+  if(opt_yield)
+  {
+    strcat(test_name, "-yield");
+  }
+  if(opt_sync_mutex)
+  {
+    strcat(test_name, "-m");
+  }
+  if(!(opt_yield | opt_sync_mutex))
+  {
+    strcat(test_name, "-none");
+  }
+
+
 
   fprintf(stdout, "%s,%d,%d,%lld,%lld,%lld,%lld\n", test_name, num_threads, num_iterations, num_operations, time_elapsed, time_per_operation, counter);
 
